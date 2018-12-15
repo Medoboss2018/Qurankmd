33ba51a69ed9b082b56e52e3b229f1bd909fd1d0
Medoboss2018 <45499460+Medoboss2018@users.noreply.github.com>
2018-12-15 11:44:49 +0000

    Update quran.js

--- quran.js
+++ quran.js
@@ -9,7 +9,7 @@ const yt_api_key = "AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4";
 const prefix = '$';
 
 client.on('message', message => {
-	if(message.content.startsWith(prefix + 'قرآن')) {
+	if(message.content.startsWith(prefix + 'قران')) {
 		message.delete();
     const voiceChannel = message.member.voiceChannel;
     if (!voiceChannel) return message.reply(`**يحب ان تكون في روم صوتي**`);
